101db6c15c3e481ad0f067f3ed65651dbcca19b2
erduo <94069625@qq.com>
2013-03-06 20:35:00 +0800

    Update CCActionCatmullRom.h
    一个运动，都要经过一段距离或者时间。 有一系列的控制点构成。 

--- cocos2d/CCActionCatmullRom.h
+++ cocos2d/CCActionCatmullRom.h
@@ -36,6 +36,7 @@
 #import "CCActionInterval.h"
 
 /** An Array that contain control points.
+一个数组，即控制点； 可以这样理解：从一个运动点到下一个点构成
  Used by CCCardinalSplineTo and (By) and CCCatmullRomTo (and By) actions.
  */
 @interface CCPointArray : NSObject <NSCopying>
@@ -43,41 +44,43 @@
 	NSMutableArray *controlPoints_;
 }
 
-/** Array that contains the control points */
+/** Array that contains the control points  数组就是控制点*/
 @property (nonatomic,readwrite,retain) NSMutableArray *controlPoints;
 
-/** creates and initializes a Points array with capacity */
+/** creates and initializes a Points array with capacity 
+创建并初始化一个数组，使用容量值
+*/
  +(id) arrayWithCapacity:(NSUInteger)capacity;
 
-/** initializes a Catmull Rom config with a capacity hint */
+/** initializes a Catmull Rom config with a capacity hint 使用容量来初始化 */
 -(id) initWithCapacity:(NSUInteger)capacity;
 
-/** appends a control point */
+/** appends a control point 增加一个控制点*/
 -(void) addControlPoint:(CGPoint)controlPoint;
 
-/** inserts a controlPoint at index */
+/** inserts a controlPoint at index 插入一个控制点的次序 */
 -(void) insertControlPoint:(CGPoint)controlPoint atIndex:(NSUInteger)index;
 
-/** replaces an existing controlPoint at index */
+/** replaces an existing controlPoint at index 替换一个控制点 */
 -(void) replaceControlPoint:(CGPoint)controlPoint atIndex:(NSUInteger)index;
 
-/** get the value of a controlPoint at a given index */
+/** get the value of a controlPoint at a given index  获取一个控制点的位置*/
 -(CGPoint) getControlPointAtIndex:(NSInteger)index;
 
-/** deletes a control point at a given index */
+/** deletes a control point at a given index  删除指定的控制点*/
 -(void) removeControlPointAtIndex:(NSUInteger)index;
 
-/** returns the number of objects of the control point array */
+/** returns the number of objects of the control point array 返回个数 */
 -(NSUInteger) count;
 
-/** returns a new copy of the array reversed. User is responsible for releasing this copy */
+/** returns a new copy of the array reversed. User is responsible for releasing this copy  返回一组新的逆向控制点，用户负责要释放掉*/
 -(CCPointArray*) reverse;
 
-/** reverse the current control point array inline, without generating a new one */
+/** reverse the current control point array inline, without generating a new one  就是将当前的控制点转向，不会产生新的控制点*/
 -(void) reverseInline;
 @end
 
-/** Cardinal Spline path.
+/** Cardinal Spline path. 运动的路径？
  http://en.wikipedia.org/wiki/Cubic_Hermite_spline#Cardinal_spline
  */
 @interface CCCardinalSplineTo : CCActionInterval
@@ -87,13 +90,15 @@
 	CGFloat			tension_;
 }
 
-/** Array of control points */
+/** Array of control points  一组控制点*/
  @property (nonatomic,readwrite,retain) CCPointArray *points;
 
-/** creates an action with a Cardinal Spline array of points and tension */
+/** creates an action with a Cardinal Spline array of points and tension
+使用阵列点和力来创建一个运动
+ */
 +(id) actionWithDuration:(ccTime)duration points:(CCPointArray*)points tension:(CGFloat)tension;
 
-/** initializes the action with a duration and an array of points */
+/** initializes the action with a duration and an array of points    使用间隔和控制点来初始化运动*/
 -(id) initWithDuration:(ccTime)duration points:(CCPointArray*)points tension:(CGFloat)tension;
 
 @end
@@ -110,25 +115,26 @@
 /** An action that moves the target with a CatmullRom curve to a destination point.
  A Catmull Rom is a Cardinal Spline with a tension of 0.5.
  http://en.wikipedia.org/wiki/Cubic_Hermite_spline#Catmull.E2.80.93Rom_spline
+一个运动就是讲目标移动到目的地。
  */
 @interface CCCatmullRomTo : CCCardinalSplineTo
 {
 }
-/** creates an action with a Cardinal Spline array of points and tension */
+/** creates an action with a Cardinal Spline array of points and tension 创建于东使用了cardinal spline就是阵列点 */
 +(id) actionWithDuration:(ccTime)dt points:(CCPointArray*)points;
 
-/** initializes the action with a duration and an array of points */
+/** initializes the action with a duration and an array of points 使用间隔和控制点来初始化运动 */
 -(id) initWithDuration:(ccTime)dt points:(CCPointArray*)points;
 @end
 
-/** An action that moves the target with a CatmullRom curve by a certain distance.
+/** An action that moves the target with a CatmullRom curve by a certain distance.   一个运动移动目标，要经过移动距离
   A Catmull Rom is a Cardinal Spline with a tension of 0.5.
  http://en.wikipedia.org/wiki/Cubic_Hermite_spline#Catmull.E2.80.93Rom_spline
  */
 @interface CCCatmullRomBy : CCCardinalSplineBy
 {
 }
-/** creates an action with a Cardinal Spline array of points and tension */
+/** creates an action with a Cardinal Spline array of points and tension  一如同上解释 */
 +(id) actionWithDuration:(ccTime)dt points:(CCPointArray*)points;
 
 /** initializes the action with a duration and an array of points */
