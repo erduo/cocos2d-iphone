3c16534a49a99368e28d9bb31f8d314cca4c00e8
erduo <94069625@qq.com>
2013-03-07 10:50:14 +0800

    Update CCActionInstant.h
    及时行动：展示，隐藏；x,y轴翻动，放置位置，回调，回调中带有节点或对象以及块中执行等
    方法的定义

--- cocos2d/CCActionInstant.h
+++ cocos2d/CCActionInstant.h
@@ -29,13 +29,14 @@
 
 /** Instant actions are immediate actions. They don't have a duration like
  the CCIntervalAction actions.
+及时行动的方法，不具有时间周期
 */
 @interface CCActionInstant : CCFiniteTimeAction <NSCopying>
 {
 }
 @end
 
-/** Show the node
+/** Show the node   展示
  */
  @interface CCShow : CCActionInstant
 {
@@ -49,14 +50,14 @@
 }
 @end
 
-/** Toggles the visibility of a node
+/** Toggles the visibility of a node   切换可见性
  */
 @interface CCToggleVisibility : CCActionInstant
 {
 }
 @end
 
-/** Flips the sprite horizontally
+/** Flips the sprite horizontally  水平翻动 是否是
  @since v0.99.0
  */
 @interface CCFlipX : CCActionInstant
@@ -67,7 +68,7 @@
 -(id) initWithFlipX:(BOOL)x;
 @end
 
-/** Flips the sprite vertically
+/** Flips the sprite vertically   垂直参数
  @since v0.99.0
  */
 @interface CCFlipY : CCActionInstant
@@ -78,19 +79,19 @@
 -(id) initWithFlipY:(BOOL)y;
 @end
 
-/** Places the node in a certain position
+/** Places the node in a certain position  指定位置参数
  */
 @interface CCPlace : CCActionInstant <NSCopying>
 {
 	CGPoint position;
 }
-/** creates a Place action with a position */
+/** creates a Place action with a position 创建和初始化一个指定位置的及时运动 */
 +(id) actionWithPosition: (CGPoint) pos;
 /** Initializes a Place action with a position */
 -(id) initWithPosition: (CGPoint) pos;
 @end
 
-/** Calls a 'callback'
+/** Calls a 'callback'   响应一个回调
  */
 @interface CCCallFunc : CCActionInstant <NSCopying>
 {
@@ -98,19 +99,19 @@
 	SEL selector_;
 }
 
-/** Target that will be called */
+/** Target that will be called   回调的目标 */
 @property (nonatomic, readwrite, retain) id targetCallback;
 
-/** creates the action with the callback */
+/** creates the action with the callback  创建和初始化方法 */
 +(id) actionWithTarget: (id) t selector:(SEL) s;
 /** initializes the action with the callback */
 -(id) initWithTarget: (id) t selector:(SEL) s;
-/** exeuctes the callback */
+/** exeuctes the callback  执行回调 */
 -(void) execute;
 @end
 
 /** Calls a 'callback' with the node as the first argument.
- N means Node
+ N means Node   用节点作为第一参数的回调
  */
 @interface CCCallFuncN : CCCallFunc
 {
@@ -118,8 +119,9 @@
 @end
 
 typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
-/** Calls a 'callback' with the node as the first argument and the 2nd argument is data.
+/** Calls a 'callback' with the node as the first argument and the 2nd argument is data. 第二参数为数据
  * ND means: Node and Data. Data is void *, so it could be anything.
+节点和数据，可以是任何对象
  */
 @interface CCCallFuncND : CCCallFuncN
 {
@@ -127,27 +129,32 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 	CC_CALLBACK_ND	callbackMethod_;
 }
 
-/** Invocation object that has the target#selector and the parameters */
+/** Invocation object that has the target#selector and the parameters 
+调用的对象具有 目标 选择器和参数
+*/
 @property (nonatomic,readwrite) CC_CALLBACK_ND callbackMethod;
 
-/** creates the action with the callback and the data to pass as an argument */
+/** creates the action with the callback and the data to pass as an argument
+创建带有回调数据作为参数的运动 */
 +(id) actionWithTarget: (id) t selector:(SEL) s data:(void*)d;
 /** initializes the action with the callback and the data to pass as an argument */
 -(id) initWithTarget:(id) t selector:(SEL) s data:(void*) d;
 @end
 
 /** Calls a 'callback' with an object as the first argument.
- O means Object.
+ O means Object.   对象作为参数的回调
  @since v0.99.5
  */
 @interface CCCallFuncO : CCCallFunc
 {
 	id	object_;
 }
-/** object to be passed as argument */
+/** object to be passed as argument  对象做为参数*/
 @property (nonatomic, readwrite, retain) id object;
 
-/** creates the action with the callback and the object to pass as an argument */
+/** creates the action with the callback and the object to pass as an argument
+创建和初始化一个带有对象参数的回调
+ */
 +(id) actionWithTarget: (id) t selector:(SEL) s object:(id)object;
 /** initializes the action with the callback and the object to pass as an argument */
 -(id) initWithTarget:(id) t selector:(SEL) s object:(id)object;
@@ -156,7 +163,7 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 
 #pragma mark Blocks Support
 
-/** Executes a callback using a block.
+/** Executes a callback using a block.   使用块来执行一个回调
  */
 @interface CCCallBlock : CCActionInstant<NSCopying>
 {
@@ -165,6 +172,7 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 
 /** creates the action with the specified block, to be used as a callback.
  The block will be "copied".
+使用指定的块来执行回调。该块是可复制的。 下面是初始化方法，以及执行方法
  */
 +(id) actionWithBlock:(void(^)())block;
 
@@ -180,6 +188,7 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 @class CCNode;
 
 /** Executes a callback using a block with a single CCNode parameter.
+使用带有单个节点参数的块来执行回调
  */
 @interface CCCallBlockN : CCActionInstant<NSCopying>
 {
@@ -188,6 +197,7 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 
 /** creates the action with the specified block, to be used as a callback.
  The block will be "copied".
+同上解释，块是指定的，是可复制的
  */
 +(id) actionWithBlock:(void(^)(CCNode *node))block;
 
@@ -202,6 +212,7 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 
 /** Executes a callback using a block with a single NSObject parameter.
  @since v2.0
+执行带有单个对象参数的块的回调
  */
 @interface CCCallBlockO : CCActionInstant<NSCopying>
 {
@@ -209,11 +220,12 @@ typedef void (*CC_CALLBACK_ND)(id, SEL, id, void *);
 	id object_;
 }
 
-/** object to be passed to the block */
+/** object to be passed to the block 对象作为参数 */
 @property (nonatomic,retain) id object;
 
 /** creates the action with the specified block, to be used as a callback.
  The block will be "copied".
+同上
  */
 +(id) actionWithBlock:(void(^)(id object))block object:(id)object;
 
