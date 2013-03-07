4202c7a4d55dad63375c5211c85d1739d9aed7d3
erduo <94069625@qq.com>
2013-03-07 22:14:06 +0800

    Update CCLayer.h
    层的定义，主要是用来接收触摸和加速计输入事件；扩展了颜色层：透明度和颜色；还有2个其他层的
    扩展

--- cocos2d/CCLayer.h
+++ cocos2d/CCLayer.h
@@ -42,7 +42,8 @@
 #pragma mark CCLayer
 
 /** CCLayer is a subclass of CCNode that implements the CCTouchEventsDelegate protocol.
-
+层是CCNode的子类，用来实现CCTouchEventsDelegate 协议
+具有所有CCNode的特征，增加了可以接收触摸和加速计输入事件
  All features from CCNode are valid, plus the following new features:
  - It can receive iPhone Touches
  - It can receive Accelerometer input
@@ -55,8 +56,9 @@
 }
 /** If isTouchEnabled, this method is called onEnter. Override it to change the
  way CCLayer receives touch events.
+启用触摸，重载onEnter方法，改变接收触摸事件
  ( Default: [touchDispatcher addStandardDelegate:self priority:0] )
- Example:
+ Example:  注册触摸调度
      -(void) registerWithTouchDispatcher
      {
         [touchDispatcher addTargetedDelegate:self priority:INT_MIN+1 swallowsTouches:YES];
@@ -73,7 +75,7 @@
  Only the touches of this node will be affected. This "method" is not propagated to its children.
 
  Valid on iOS and Mac OS X v10.6 and later.
-
+是否接收触摸事件。 
  @since v0.8.1
  */
 @property(nonatomic,assign) BOOL isTouchEnabled;
@@ -81,7 +83,7 @@
  You can enable / disable accelerometer events with this property.
 
  Valid only on iOS. Not valid on Mac.
-
+是否接收加速计事件
  @since v0.8.1
  */
 @property(nonatomic,assign) BOOL isAccelerometerEnabled;
@@ -91,9 +93,9 @@
 
 @interface CCLayer : CCNode <CCKeyboardEventDelegate, CCMouseEventDelegate, CCTouchEventDelegate>
 {
-	BOOL	isMouseEnabled_;
-	BOOL	isKeyboardEnabled_;
-	BOOL	isTouchEnabled_;
+	BOOL	isMouseEnabled_;//鼠标
+	BOOL	isKeyboardEnabled_;//键盘
+	BOOL	isTouchEnabled_;//触摸
 }
 
 /** whether or not it will receive mouse events.
@@ -117,7 +119,7 @@
 /** priority of the mouse event delegate.
  Default 0.
  Override this method to set another priority.
-
+鼠标事件委托
  Valind only Mac. Not valid on iOS
  */
 -(NSInteger) mouseDelegatePriority;
@@ -125,7 +127,7 @@
 /** priority of the keyboard event delegate.
  Default 0.
  Override this method to set another priority.
-
+键盘事件
  Valind only Mac. Not valid on iOS
  */
 -(NSInteger) keyboardDelegatePriority;
@@ -147,7 +149,7 @@
 #pragma mark CCLayerColor
 
 /** CCLayerColor is a subclass of CCLayer that implements the CCRGBAProtocol protocol.
-
+颜色层是层的子类，实现CCRGBAProtocol协议。继承所有层的特征，增加了透明度和rgb颜色
  All features from CCLayer are valid, plus the following new features:
  - opacity
  - RGB colors
@@ -162,19 +164,19 @@
 	ccBlendFunc	blendFunc_;
 }
 
-/** creates a CCLayer with color, width and height in Points*/
+/** creates a CCLayer with color, width and height in Points 使用颜色，宽和高来创建*/
 + (id) layerWithColor: (ccColor4B)color width:(GLfloat)w height:(GLfloat)h;
 /** creates a CCLayer with color. Width and height are the window size. */
 + (id) layerWithColor: (ccColor4B)color;
 
 /** initializes a CCLayer with color, width and height in Points.
- This is the designated initializer.
+ This is the designated initializer.  指定初始化
  */
 - (id) initWithColor:(ccColor4B)color width:(GLfloat)w height:(GLfloat)h;
 /** initializes a CCLayer with color. Width and height are the window size. */
 - (id) initWithColor:(ccColor4B)color;
 
-/** change width in Points */
+/** change width in Points  改变宽 */
 -(void) changeWidth: (GLfloat)w;
 /** change height in Points */
 -(void) changeHeight: (GLfloat)h;
@@ -183,7 +185,7 @@
  */
 -(void) changeWidth:(GLfloat)w height:(GLfloat)h;
 
-/** Opacity: conforms to CCRGBAProtocol protocol */
+/** Opacity: conforms to CCRGBAProtocol protocol 透明度：符合CCRGBAProtocol 协议*/
 @property (nonatomic,readonly) GLubyte opacity;
 /** Opacity: conforms to CCRGBAProtocol protocol */
 @property (nonatomic,readonly) ccColor3B color;
@@ -196,7 +198,7 @@
 
 /** CCLayerGradient is a subclass of CCLayerColor that draws gradients across
 the background.
-
+是颜色层的子类，用来绘制梯度背景；继承了颜色层所有特征，增加了方向，最终颜色，插补模式
  All features from CCLayerColor are valid, plus the following new features:
  - direction
  - final color
@@ -205,13 +207,13 @@ the background.
  Color is interpolated between the startColor and endColor along the given
  vector (starting at the origin, ending at the terminus).  If no vector is
  supplied, it defaults to (0, -1) -- a fade from top to bottom.
-
+颜色是从开始色到结束色给定一个向量加上插补。默认向量为(0,-1）
  If 'compressedInterpolation' is disabled, you will not see either the start or end color for
  non-cardinal vectors; a smooth gradient implying both end points will be still
  be drawn, however.
 
  If ' compressedInterpolation' is enabled (default mode) you will see both the start and end colors of the gradient.
-
+如果compressedInterpolation此参数被禁用，看不到两端的颜色，及变化过程。没有禁用可以看到
  @since v0.99.5
  */
 @interface CCLayerGradient : CCLayerColor
@@ -223,7 +225,7 @@ the background.
 	BOOL	compressedInterpolation_;
 }
 
-/** Creates a full-screen CCLayer with a gradient between start and end. */
+/** Creates a full-screen CCLayer with a gradient between start and end. 使用开始和结束色来创建 ； 下面增加向量 */
 + (id) layerWithColor: (ccColor4B) start fadingTo: (ccColor4B) end;
 /** Creates a full-screen CCLayer with a gradient between start and end in the direction of v. */
 + (id) layerWithColor: (ccColor4B) start fadingTo: (ccColor4B) end alongVector: (CGPoint) v;
@@ -233,18 +235,19 @@ the background.
 /** Initializes the CCLayer with a gradient between start and end in the direction of v. */
 - (id) initWithColor: (ccColor4B) start fadingTo: (ccColor4B) end alongVector: (CGPoint) v;
 
-/** The starting color. */
+/** The starting color.  开始色 */
 @property (nonatomic, readwrite) ccColor3B startColor;
-/** The ending color. */
+/** The ending color.  结束色*/
 @property (nonatomic, readwrite) ccColor3B endColor;
-/** The starting opacity. */
+/** The starting opacity. 开始的透明度 */
 @property (nonatomic, readwrite) GLubyte startOpacity;
-/** The ending color. */
+/** The ending color.  结束的透明度 */
 @property (nonatomic, readwrite) GLubyte endOpacity;
-/** The vector along which to fade color. */
+/** The vector along which to fade color.  渐变色的矢量 */
 @property (nonatomic, readwrite) CGPoint vector;
 /** Whether or not the interpolation will be compressed in order to display all the colors of the gradient both in canonical and non canonical vectors
  Default: YES
+默认为是，压缩插值
  */
 @property (nonatomic, readwrite) BOOL compressedInterpolation;
 
@@ -254,6 +257,7 @@ the background.
 #pragma mark CCLayerMultiplex
 
 /** CCLayerMultiplex is a CCLayer with the ability to multiplex its children.
+是层的子类，可以复合多个子类，支持多个子类，同一时刻只有一个是激活状态
  Features:
    - It supports one or more children
    - Only one children will be active a time
@@ -264,16 +268,18 @@ the background.
 	NSMutableArray *layers_;
 }
 
-/** creates a CCMultiplexLayer with one or more layers using a variable argument list. */
+/** creates a CCMultiplexLayer with one or more layers using a variable argument list.   使用链表 */
 +(id) layerWithLayers: (CCLayer*) layer, ... NS_REQUIRES_NIL_TERMINATION;
 /** initializes a MultiplexLayer with one or more layers using a variable argument list. */
 -(id) initWithLayers: (CCLayer*) layer vaList:(va_list) params;
 /** switches to a certain layer indexed by n.
  The current (old) layer will be removed from its parent with 'cleanup:YES'.
+转到指定的层。旧的层被删除和从父类中释放
  */
 -(void) switchTo: (unsigned int) n;
 /** release the current layer and switches to another layer indexed by n.
  The current (old) layer will be removed from its parent with 'cleanup:YES'.
+释放当前层，转到n层
  */
 -(void) switchToAndReleaseMe: (unsigned int) n;
 @end
