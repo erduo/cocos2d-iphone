1058261e060e02a05cfb50285683c4d267086f6d
erduo <94069625@qq.com>
2013-03-08 11:45:25 +0800

    Update ccShader_PositionTexture_uColor_frag.h
    因为顶点着色器是管线中的第一个组件，我们传递属性至位置和颜色，并让其通过内部变量
    （改变v_color）传递颜色属性至片段着色器。

--- cocos2d/ccShader_PositionTexture_uColor_frag.h
+++ cocos2d/ccShader_PositionTexture_uColor_frag.h
@@ -8,7 +8,7 @@ uniform		vec4 u_color;				\n\
 varying vec2 v_texCoord;				\n\
 										\n\
 uniform sampler2D u_texture;			\n\
-										\n\
+//因为顶点着色器是管线中的第一个组件，我们传递属性至位置和颜色，并让其通过内部变量（改变v_color）传递颜色属性至片段着色器。\n\
 void main()								\n\
 {										\n\
 	gl_FragColor =  texture2D(u_texture, v_texCoord) * u_color;	\n\
