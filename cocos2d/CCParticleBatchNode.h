24c8bfac26d166c8692de52744bb0ff359a5046d
erduo <94069625@qq.com>
2013-03-08 20:16:54 +0800

    批量粒子节点

--- cocos2d/CCParticleBatchNode.h
+++ cocos2d/CCParticleBatchNode.h
@@ -32,25 +32,25 @@
 @class CCTextureAtlas;
 @class CCParticleSystem;
 
-//don't use lazy sorting for particle systems
+//don't use lazy sorting for particle systems  不用使用懒惰的粒子存储系统
 @interface CCNode (extension)
 -(void) setZOrder:(NSUInteger) z;
 @end
 
 /** CCParticleBatchNode is like a batch node: if it contains children, it will draw them in 1 single OpenGL call
  * (often known as "batch draw").
- *
- * A CCParticleBatchNode can reference one and only one texture (one image file, one texture atlas).
- * Only the CCParticleSystems that are contained in that texture can be added to the CCSpriteBatchNode.
- * All CCParticleSystems added to a CCSpriteBatchNode are drawn in one OpenGL ES draw call.
+ *一个批量节点。单次调用opengl
+ * A CCParticleBatchNode can reference one and only one texture (one image file, one texture atlas).只能引用一个只有一个纹理（图片，纹理地图）
+ * Only the CCParticleSystems that are contained in that texture can be added to the CCSpriteBatchNode.  只有CCParticeSystem可以添加给精灵批量节点
+ * All CCParticleSystems added to a CCSpriteBatchNode are drawn in one OpenGL ES draw call.  也是一次调用
  * If the CCParticleSystems are not added to a CCParticleBatchNode then an OpenGL ES draw call will be needed for each one, which is less efficient.
+ *如果没有添加，每次回调用opengl，这样效率很低
  *
- *
- * Limitations:
+ * Limitations:限制：某时刻只有CCParticleSystemQuad 是被支持的；所有系统都要带有相同的参数
  * - At the moment only CCParticleSystemQuad is supported
  * - All systems need to be drawn with the same parameters, blend function, aliasing, texture
  *
- * Most efficient usage
+ * Most efficient usage 高效使用：使用纹理和容量来初始化
  * - Initialize the ParticleBatchNode with the texture and enough capacity for all the particle systems
  * - Initialize all particle systems and add them as child to the batch node
  * @since v1.1
@@ -62,12 +62,12 @@
 	ccBlendFunc		blendFunc_;
 }
 
-/** the texture atlas used for drawing the quads */
+/** the texture atlas used for drawing the quads   为绘制四边形*/
 @property (nonatomic, retain) CCTextureAtlas* textureAtlas;
-/** the blend function used for drawing the quads */
+/** the blend function used for drawing the quads  都是为绘制四边形 */
 @property (nonatomic, readwrite) ccBlendFunc blendFunc;
 
-/** initializes the particle system with CCTexture2D, a default capacity of 500 */
+/** initializes the particle system with CCTexture2D, a default capacity of 500 默认容量为500，来初始化*/
 +(id)batchNodeWithTexture:(CCTexture2D *)tex;
 
 /** initializes the particle system with the name of a file on disk (for a list of supported formats look at the CCTexture2D class), a default capacity of 500 particles */
@@ -88,12 +88,12 @@
 /** Add a child into the CCParticleBatchNode */
 -(void) addChild:(CCParticleSystem*)child z:(NSInteger)z tag:(NSInteger) aTag;
 
-/** Inserts a child into the CCParticleBatchNode */
+/** Inserts a child into the CCParticleBatchNode  插入子节点 */
 -(void) insertChild:(CCParticleSystem*) pSystem inAtlasAtIndex:(NSUInteger)index;
 
-/** remove child from the CCParticleBatchNode */
+/** remove child from the CCParticleBatchNode  删除 */
 -(void) removeChild:(CCParticleSystem*) pSystem cleanup:(BOOL)doCleanUp;
 
-/** disables a particle by inserting a 0'd quad into the texture atlas */
+/** disables a particle by inserting a 0'd quad into the texture atlas  是一个子节点失效*/
 -(void) disableParticle:(NSUInteger) particleIndex;
 @end
