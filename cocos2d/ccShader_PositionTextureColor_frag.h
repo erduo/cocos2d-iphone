8e6583d4b09e123a359cff76568cd387861b54ba
erduo <94069625@qq.com>
2013-03-08 11:36:06 +0800

    Update ccShader_PositionTextureColor_frag.h
    插入到c++中需要增加换行符

--- cocos2d/ccShader_PositionTextureColor_frag.h
+++ cocos2d/ccShader_PositionTextureColor_frag.h
@@ -6,7 +6,7 @@ precision lowp float;						\n\
 varying vec4 v_fragmentColor;				\n\
 varying vec2 v_texCoord;					\n\
 uniform sampler2D u_texture;				\n\
-											\n\
+			 								\n\
 void main()									\n\
 {											\n\
 	gl_FragColor = v_fragmentColor * texture2D(u_texture, v_texCoord);			\n\
