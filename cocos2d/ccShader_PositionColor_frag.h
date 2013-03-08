dcae465380fe53d1fe69d144526aec9566e54faf
erduo <94069625@qq.com>
2013-03-08 11:18:45 +0800

    Update ccShader_PositionColor_frag.h
    需要测试使用

--- cocos2d/ccShader_PositionColor_frag.h
+++ cocos2d/ccShader_PositionColor_frag.h
@@ -4,7 +4,7 @@ precision lowp float;								\n\
 #endif												\n\
 													\n\
 varying vec4 v_fragmentColor;						\n\
-													\n\
+//还不知道怎么用的,片段颜色													\n\
 void main()											\n\
 {													\n\
 	gl_FragColor = v_fragmentColor;					\n\
