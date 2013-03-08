ae0c19e39d7c6a1ec3ee0074ae99c03c84301270
erduo <94069625@qq.com>
2013-03-08 11:43:10 +0800

    Update ccShader_PositionTexture_frag.h
    无论什么程序转换/效果，每个顶点的最终位置靠分配坐标至gl_Position进行设置；
    另一方面，每个像素的最终颜色通过指定颜色至gl_FragColor进行设置。

--- cocos2d/ccShader_PositionTexture_frag.h
+++ cocos2d/ccShader_PositionTexture_frag.h
@@ -5,8 +5,8 @@ precision lowp float;													\n\
 																		\n\
 varying vec2 v_texCoord;												\n\
 uniform sampler2D u_texture;											\n\
-																		\n\
-void main()																\n\
+//无论什么程序转换/效果，每个顶点的最终位置靠分配坐标至gl_Position进行设置；另一方面，每个像素的最终颜色通过指定颜色至gl_FragColor进行设置。	\n\
+void main()							\n\
 {																		\n\
 	gl_FragColor =  texture2D(u_texture, v_texCoord);					\n\
 }																		\n\
