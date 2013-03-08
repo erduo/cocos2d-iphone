5a6b71e273d255e9296e1f86e400b7c025416367
erduo <94069625@qq.com>
2013-03-08 11:49:55 +0800

    Update ccShader_Position_uColor_frag.h
    指定片段颜色

--- cocos2d/ccShader_Position_uColor_frag.h
+++ cocos2d/ccShader_Position_uColor_frag.h
@@ -4,7 +4,7 @@ precision lowp float;					\n\
 #endif									\n\
 										\n\
 varying vec4 v_fragmentColor;			\n\
-										\n\
+//指定片段颜色			\n\
 void main()								\n\
 {										\n\
 	gl_FragColor = v_fragmentColor;		\n\
