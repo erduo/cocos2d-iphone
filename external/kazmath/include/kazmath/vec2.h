c25f6ae2dcfbaf294fcb87cfad55fce82ce194be
erduo <94069625@qq.com>
2013-03-09 19:48:03 +0800

    Vec2(3.0,4.0)

--- external/kazmath/include/kazmath/vec2.h
+++ external/kazmath/include/kazmath/vec2.h
@@ -32,7 +32,7 @@ struct kmMat3;
 #define kmScalar float
 #endif
 
-#pragma pack(push)  /* push current alignment to stack */
+#pragma pack(push)  /* push current alignment to stack 推人当前对齐到栈中*/
 #pragma pack(1)     /* set alignment to 1 byte boundary */
 typedef struct kmVec2 {
     kmScalar x;
@@ -45,8 +45,8 @@ typedef struct kmVec2 {
 extern "C" {
 #endif
 kmVec2* kmVec2Fill(kmVec2* pOut, kmScalar x, kmScalar y);
-kmScalar kmVec2Length(const kmVec2* pIn); ///< Returns the length of the vector
-kmScalar kmVec2LengthSq(const kmVec2* pIn); ///< Returns the square of the length of the vector
+kmScalar kmVec2Length(const kmVec2* pIn); ///< Returns the length of the vector 返回矢量的长度
+kmScalar kmVec2LengthSq(const kmVec2* pIn); ///< Returns the square of the length of the vector 平方
 kmVec2* kmVec2Normalize(kmVec2* pOut, const kmVec2* pIn); ///< Returns the vector passed in set to unit length
 kmVec2* kmVec2Add(kmVec2* pOut, const kmVec2* pV1, const kmVec2* pV2); ///< Adds 2 vectors and returns the result
 kmScalar kmVec2Dot(const kmVec2* pV1, const kmVec2* pV2); /** Returns the Dot product which is the cosine of the angle between the two vectors multiplied by their lengths */
@@ -54,7 +54,7 @@ kmVec2* kmVec2Subtract(kmVec2* pOut, const kmVec2* pV1, const kmVec2* pV2); ///<
 kmVec2* kmVec2Transform(kmVec2* pOut, const kmVec2* pV1, const struct kmMat3* pM); /** Transform the Vector */
 kmVec2* kmVec2TransformCoord(kmVec2* pOut, const kmVec2* pV, const struct kmMat3* pM); ///<Transforms a 2D vector by a given matrix, projecting the result back into w = 1.
 kmVec2* kmVec2Scale(kmVec2* pOut, const kmVec2* pIn, const kmScalar s); ///< Scales a vector to length s
-int	kmVec2AreEqual(const kmVec2* p1, const kmVec2* p2); ///< Returns 1 if both vectors are equal
+int	kmVec2AreEqual(const kmVec2* p1, const kmVec2* p2); ///< Returns 1 if both vectors are equal 相等则返回1
 
 #ifdef __cplusplus
 }
