63c3d9b39af8e39b3a2a8fd4c67c1d1d13d11492
erduo <94069625@qq.com>
2013-03-10 15:25:52 +0800

    瓦片层

--- cocos2d/CCTMXLayer.h
+++ cocos2d/CCTMXLayer.h
@@ -25,7 +25,7 @@
  *
  * TMX Tiled Map support:
  * http://www.mapeditor.org
- *
+ * TMX:文本文件
  */
 
 
@@ -38,8 +38,8 @@
 @class CCTMXTilesetInfo;
 
 
-/** CCTMXLayer represents the TMX layer.
-
+/** CCTMXLayer represents the TMX layer. 文本层
+是批量节点的子类，默认用纹理集合呈现；运行时修改一个瓦片，变为一个精灵，否则没有对象可以创建；可以使用旋转，缩放，移动方法
  It is a subclass of CCSpriteBatchNode. By default the tiles are rendered using a CCTextureAtlas.
  If you mofify a tile on runtime, then, that tile will become a CCSprite, otherwise no CCSprite objects are created.
  The benefits of using CCSprite objects as tiles are:
@@ -58,7 +58,7 @@
  @since v0.8.1
  
  Tiles can have tile flags for additional properties. At the moment only flip horizontal and flip vertical are used. These bit flags are defined in CCTMXXMLParser.h.
- 
+ 瓦片具有标志属性；某一时刻，只有垂直和水平翻转使用，在CCTMXXMLParser.h有定义
  @since 1.1
  */
 @interface CCTMXLayer : CCSpriteBatchNode
@@ -67,11 +67,11 @@
 	NSString			*layerName_;
 	CGSize				layerSize_;
 	CGSize				mapTileSize_;
-	uint32_t			*tiles_;			// GID are 32 bit
+	uint32_t			*tiles_;			// GID are 32 bit GID: 群体身份（指共享资源系统使用者的身份）
 	NSUInteger			layerOrientation_;
 	NSMutableArray		*properties_;
 
-	unsigned char		opacity_; // TMX Layer supports opacity
+	unsigned char		opacity_; // TMX Layer supports opacity 容量
 
 	NSUInteger			minGID_;
 	NSUInteger			maxGID_;
@@ -80,31 +80,31 @@
 	NSInteger			vertexZvalue_;
 	BOOL				useAutomaticVertexZ_;
 
-	// used for optimization
+	// used for optimization 用于优化
 	CCSprite		*reusedTile_;
 	ccCArray		*atlasIndexArray_;
 }
-/** name of the layer */
+/** name of the layer 层名称*/
 @property (nonatomic,readwrite,retain) NSString *layerName;
-/** size of the layer in tiles */
+/** size of the layer in tiles 层大小*/
 @property (nonatomic,readwrite) CGSize layerSize;
-/** size of the map's tile (could be differnt from the tile's size) */
+/** size of the map's tile (could be differnt from the tile's size) 大小 */
 @property (nonatomic,readwrite) CGSize mapTileSize;
-/** pointer to the map of tiles */
+/** pointer to the map of tiles 瓦片地图指针 */
 @property (nonatomic,readwrite) uint32_t *tiles;
-/** Tilset information for the layer */
+/** Tilset information for the layer  信息*/
 @property (nonatomic,readwrite,retain) CCTMXTilesetInfo *tileset;
-/** Layer orientation, which is the same as the map orientation */
+/** Layer orientation, which is the same as the map orientation 方向*/
 @property (nonatomic,readwrite) NSUInteger layerOrientation;
-/** properties from the layer. They can be added using Tiled */
+/** properties from the layer. They can be added using Tiled 属性 */
 @property (nonatomic,readwrite,retain) NSMutableArray *properties;
 
 /** creates a CCTMXLayer with an tileset info, a layer info and a map info */
 +(id) layerWithTilesetInfo:(CCTMXTilesetInfo*)tilesetInfo layerInfo:(CCTMXLayerInfo*)layerInfo mapInfo:(CCTMXMapInfo*)mapInfo;
-/** initializes a CCTMXLayer with a tileset info, a layer info and a map info */
+/** initializes a CCTMXLayer with a tileset info, a layer info and a map info  创建和初始化*/
 -(id) initWithTilesetInfo:(CCTMXTilesetInfo*)tilesetInfo layerInfo:(CCTMXLayerInfo*)layerInfo mapInfo:(CCTMXMapInfo*)mapInfo;
 
-/** dealloc the map that contains the tile position from memory.
+/** dealloc the map that contains the tile position from memory. 释放；除非你知道运行时瓦片的位置，才调用此方法；你可以使用[layer tileGIDAt:]
  Unless you want to know at runtime the tiles positions, you can safely call this method.
  If you are going to call [layer tileGIDAt:] then, don't release the map
  */
@@ -113,6 +113,8 @@
 /** returns the tile (CCSprite) at a given a tile coordinate.
  The returned CCSprite will be already added to the CCTMXLayer. Don't add it again.
  The CCSprite can be treated like any other CCSprite: rotated, scaled, translated, opacity, color, etc.
+ 返回给定坐标的瓦片；已经添加上的，不用再次添加；返回的精灵可以入其他精灵一样可以选择，缩放，转换，透明度，颜色；
+ 同样也可以删除
  You can remove either by calling:
 	- [layer removeChild:sprite cleanup:cleanup];
 	- or [layer removeTileAt:ccp(x,y)];
@@ -122,15 +124,17 @@
 /** returns the tile gid at a given tile coordinate.
  if it returns 0, it means that the tile is empty.
  This method requires the the tile map has not been previously released (eg. don't call [layer releaseMap])
+ 返回gid；0标示没有；瓦片地图没有被释放，不用调用[layer releaseMap]
  */
 -(uint32_t) tileGIDAt:(CGPoint)tileCoordinate;
 
 /** returns the tile gid at a given tile coordinate. It also returns the tile flags.
  This method requires the the tile map has not been previously released (eg. don't call [layer releaseMap])
+ 标志
  */
 -(uint32_t) tileGIDAt:(CGPoint)pos withFlags:(ccTMXTileFlags*)flags;
 
-/** sets the tile gid (gid = tile global id) at a given tile coordinate.
+/** sets the tile gid (gid = tile global id 瓦片全局序号) at a given tile coordinate.
  The Tile GID can be obtained by using the method "tileGIDAt" or by using the TMX editor -> Tileset Mgr +1.
  If a tile is already placed at that position, then it will be removed.
  */
@@ -145,19 +149,19 @@
 
 -(void) setTileGID:(uint32_t)gid at:(CGPoint)pos withFlags:(ccTMXTileFlags)flags;
 
-/** removes a tile at given tile coordinate */
+/** removes a tile at given tile coordinate 移除一个*/
 -(void) removeTileAt:(CGPoint)tileCoordinate;
 
 /** returns the position in points of a given tile coordinate */
 -(CGPoint) positionAt:(CGPoint)tileCoordinate;
 
-/** return the value for the specific property name */
+/** return the value for the specific property name  返回指定属性名称*/
 -(id) propertyNamed:(NSString *)propertyName;
 
-/** Creates the tiles */
+/** Creates the tiles 创建瓦片*/
 -(void) setupTiles;
 
-/** CCTMXLayer doesn't support adding a CCSprite manually.
+/** CCTMXLayer doesn't support adding a CCSprite manually. 不支持增加精灵手动；
  @warning addchild:z:tag: is not supported on CCTMXLayer. Instead of setTileGID:at:/tileAt:
  */
 -(void) addChild: (CCNode*)node z:(NSInteger)z tag:(NSInteger)tag;
