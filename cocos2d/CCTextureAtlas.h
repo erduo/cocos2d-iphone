d09a2bdb0591ba07d78efd26c00266a6457ea355
erduo <94069625@qq.com>
2013-03-10 17:50:58 +0800

    纹理地图：多个纹理

--- cocos2d/CCTextureAtlas.h
+++ cocos2d/CCTextureAtlas.h
@@ -28,7 +28,8 @@
 #import "ccTypes.h"
 #import "ccConfig.h"
 
-/** A class that implements a Texture Atlas.
+/** A class that implements a Texture Atlas. 实现纹理集；
+ * 具有属性：可以是pvr,png任意格式的纹理；更新，增加，移除，排序在运行中，容量变化，opengl组件；
  Supported features:
    * The atlas file can be a PVRTC, PNG or any other fomrat supported by Texture2D
    * Quads can be udpated in runtime
@@ -37,8 +38,9 @@
    * Quads can be re-ordered in runtime
    * The TextureAtlas capacity can be increased or decreased in runtime
    * OpenGL component: V3F, C4B, T2F.
- The quads are rendered using an OpenGL ES VBO.
+ The quads are rendered using an OpenGL ES VBO. 是使用vbo方式渲染呈现
  To render the quads using an interleaved vertex array list, you should modify the ccConfig.h file
+ 呈现quads使用在一个交错顶点数组列表，可以在配置文件中修改；
  */
 @interface CCTextureAtlas : NSObject
 {
@@ -56,23 +58,24 @@
 #endif
 }
 
-/** quantity of quads that are going to be drawn */
+/** quantity of quads that are going to be drawn  个数*/
 @property (nonatomic,readonly) NSUInteger totalQuads;
-/** quantity of quads that can be stored with the current texture atlas size */
+/** quantity of quads that can be stored with the current texture atlas size 容量*/
 @property (nonatomic,readonly) NSUInteger capacity;
-/** Texture of the texture atlas */
+/** Texture of the texture atlas 纹理地图 */
 @property (nonatomic,retain) CCTexture2D *texture;
 /** Quads that are going to be rendered */
 @property (nonatomic,readwrite) ccV3F_C4B_T2F_Quad *quads;
 
 /** creates a TextureAtlas with an filename and with an initial capacity for Quads.
  * The TextureAtlas capacity can be increased in runtime.
+ * 容量和文件名称作为参数来创建纹理地图
  */
 +(id) textureAtlasWithFile:(NSString*)file capacity:(NSUInteger)capacity;
 
 /** initializes a TextureAtlas with a filename and with a certain capacity for Quads.
  * The TextureAtlas capacity can be increased in runtime.
- *
+ *初始化。不用重复初始化！
  * WARNING: Do not reinitialize the TextureAtlas because it will leak memory (issue #706)
  */
 -(id) initWithFile: (NSString*) file capacity:(NSUInteger)capacity;
@@ -80,6 +83,7 @@
 /** creates a TextureAtlas with a previously initialized Texture2D object, and
  * with an initial capacity for n Quads.
  * The TextureAtlas capacity can be increased in runtime.
+ * 纹理和容量创建和初始化方法
  */
 +(id) textureAtlasWithTexture:(CCTexture2D *)tex capacity:(NSUInteger)capacity;
 
@@ -93,26 +97,26 @@
 
 /** updates a Quad (texture, vertex and color) at a certain index
  * index must be between 0 and the atlas capacity - 1
- @since v0.8
+ @since v0.8 更新（纹理，顶点，颜色） 带有指定序号的参数
  */
 -(void) updateQuad:(ccV3F_C4B_T2F_Quad*)quad atIndex:(NSUInteger)index;
 
 /** Inserts a Quad (texture, vertex and color) at a certain index
  index must be between 0 and the atlas capacity - 1
- @since v0.8
+ @since v0.8 插入
  */
 -(void) insertQuad:(ccV3F_C4B_T2F_Quad*)quad atIndex:(NSUInteger)index;
 
 /** Inserts a c array of quads at a given index
  index must be between 0 and the atlas capacity - 1
  this method doesn't enlarge the array when amount + index > totalQuads
- @since v1.1
+ @since v1.1 
 */
 -(void) insertQuads:(ccV3F_C4B_T2F_Quad*)quads atIndex:(NSUInteger)index amount:(NSUInteger)amount;
 
 /** Removes the quad that is located at a certain index and inserts it at a new index
  This operation is faster than removing and inserting in a quad in 2 different steps
- @since v0.7.2
+ @since v0.7.2 移除方法
 */
 -(void) insertQuadFromIndex:(NSUInteger)fromIndex atIndex:(NSUInteger)newIndex;
 
@@ -157,7 +161,7 @@
  Moves quads from index till totalQuads to the newIndex
  Used internally by CCParticleBatchNode
  This method doesn't enlarge the array if newIndex + quads to be moved > capacity
- @since 1.1
+ @since 1.1 移动
 */
 - (void) moveQuadsFromIndex:(NSUInteger) index to:(NSUInteger) newIndex;
 
@@ -168,7 +172,7 @@
 */
 - (void) fillWithEmptyQuadsFromIndex:(NSUInteger) index amount:(NSUInteger) amount;
 
-/** draws n quads
+/** draws n quads 绘制方法
  * n can't be greater than the capacity of the Atlas
  */
 
